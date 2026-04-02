46a2951b99ecec0f0c1acae5f20041cf00a7b077
lzxxth <luis.luishenrique18@tutamail.com>
2026-04-02 14:23:37 +0100

    fix: remove opts. on lspconfig.lua

--- lua/vortexnv/configs/lspconfig.lua
+++ lua/vortexnv/configs/lspconfig.lua
@@ -7,7 +7,7 @@ local servers = {
   dartls = {},
 }
 
-for server, config in pairs(opts.servers) do
+for server, config in pairs(servers) do
   config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
   lspconfig[server].setup(config)
 end
