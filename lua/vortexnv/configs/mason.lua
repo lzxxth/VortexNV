3235f7b9da317de5016326f4ddbd81e73f4141a0
lzxxth <luis.luishenrique18@tutamail.com>
2026-04-02 18:16:07 +0100

    feat: mason registries

--- lua/vortexnv/configs/mason.lua
+++ lua/vortexnv/configs/mason.lua
@@ -1,3 +1,7 @@
 require("mason").setup({
   ensure_installed = { "lua-language-server","dart","clangd","c_sharp" },
+   registries = {
+      "github:mason-org/mason-registry",     -- official Mason registry
+      "github:Crashdummyy/mason-registry",   -- custom / unofficial registry
+    },
 })
