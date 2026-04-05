a59430e51e7a8730485934d7a4da63c8bb0b7ea6
lzxxth <luis.luishenrique18@tutamail.com>
2026-04-05 03:03:40 +0100

    fix: lspconfig servers

--- lua/vortexnv/configs/lspconfig.lua
+++ lua/vortexnv/configs/lspconfig.lua
@@ -3,6 +3,12 @@ local servers = {
   clangd = {},
   dartls = {},
   jdtls = {},
+  roslyn_ls = { 
+    cmd = {
+    "dotnet",
+    "C:/Users/luis/Downloads/Microsoft.CodeAnalysis.LanguageServer.win-x64.5.4.0-2.26179.14/content/LanguageServer/win-x64/Microsoft.CodeAnalysis.LanguageServer.dll"
+  }
+ },
 }
 
 for server, config in pairs(servers) do
